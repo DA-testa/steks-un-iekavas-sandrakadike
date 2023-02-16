409fe7c9bc4de11e533cd3f6cd53dc71ba6a2994
sandrakadike <124781949+sandrakadike@users.noreply.github.com>
2023-02-16 13:59:43 +0000

    ghj

--- main.py
+++ main.py
@@ -13,19 +13,41 @@ def find_mismatch(text):
     opening_brackets_stack = []
     for i, next in enumerate(text):
         if next in "([{":
+            opening_brackets_stack.append(Bracket(next,i+1))
             # Process opening bracket, write your code here
             pass
 
         if next in ")]}":
-            # Process closing bracket, write your code here
-            pass
+            if not opening_brackets_stack or not are_matching(opening_brackets_stack[-1].char, next):
+                return (i+1)
+            opening_brackets_stack.pop()
+    if  opening_brackets_stack:
+        return opening_brackets_stack[0].position
+    return ("Success")
+            
+    # return "Success"
+    #print("ir tukss")
+    # Process closing bracket, write your code here
+    pass
+    # if (opening_brackets_stack.len(-1)):
+    #     print ("Success")
 
 
 def main():
-    text = input()
-    mismatch = find_mismatch(text)
+    izvele=(input())
+    if "I" in izvele:
+        text=input("ievadi tekstu: ")
+        # if "F" in izvele:
+        #     fname=str(input)
+        #     text= import (fname.txt)
+
+        
+        mismatch = find_mismatch(text)
+        print (mismatch)
+
     # Printing answer, write your code here
 
 
 if __name__ == "__main__":
     main()
+
